34c1331ccdab631cc52607c2cb799717ab777758
Stefan Lotties <slotties@gmail.com>
2013-02-20 21:55:33 +0100

    introduced nyaaService to handle persistency and server-calls, splitted the single controller into multiple ones

--- nyaa.js
+++ nyaa.js
@@ -1,62 +1,12 @@
 "use strict";
 
-var TorrentCtrl = function($scope, $http) {
-	var animes = loadAnimes();
-	
-	for (var i = 0; i < animes.length; i++) {
-		animes[i].nextLinks = [];
-		animes[i].latestLinks = [];
-	}
-	
-	$scope.animes = animes;
-	
-	$scope.animeFormObj = {
-		name: '',
-		episode: 2,
-		groups: [],
-		customGroup: ''
-	};
-	
-	/*
-		Actions
-	*/	
-	$scope.openAnidb = function(name) {
-		var url = 'http://anidb.net/perl-bin/animedb.pl?show=animelist&adb.search=' + escape(name) + '&do.search=search';
-		
-		chrome.tabs.update(null, { url: url });		
-	};
-	
-	$scope.move = function(idx, offset) {
-		var animes = $scope.animes;
-		if ((idx + offset) >= 0 && ((idx + offset) < animes.length)) {
-			var curr = animes[idx];
-				
-			animes.splice(idx, 1);
-			animes.splice(idx + offset, 0, curr);
+var nyaaModule = angular.module('nyaaleech', []);
+nyaaModule.factory('nyaaService', function($http) {
+	return {
+		queryTorrents: function(anime, fn) {
+			var url = 'http://www.nyaa.eu/?page=rss&cats=1_37&term=' + (anime.name + ' ' + (anime.group || anime.customGroup));
 			
-			storeAnimes($scope.animes);
-		}
-	};
-	
-	$scope.remove = function(idx) {
-		if (confirm('Really?')) {
-			$scope.animes.splice(idx, 1);
-			storeAnimes($scope.animes);
-		}
-	};
-	
-	$scope.plusOne = function(anime) {
-		anime.episode += 1;
-		
-		storeAnimes($scope.animes);
-		window.location.reload();
-	};
-	
-	$scope.pullTorrentList = function(anime) {
-		var url = 'http://www.nyaa.eu/?page=rss&cats=1_37&term=' + (anime.name + ' ' + (anime.group || anime.customGroup)),
-			el = this;
-		
-		$http({
+			$http({
 				method: 'GET',
 				url: url,
 				transformResponse: function(str) {
@@ -67,12 +17,177 @@ var TorrentCtrl = function($scope, $http) {
 			})
 			.success(function(torrentFeedXmlRoot) {
 				var xmlItems = torrentFeedXmlRoot.getElementsByTagName('item');
-				insertLinks(xmlItems, anime);
+				// FIXME: use promise instead of callback-fn
+				fn(xmlItems);
 			})
 			.error(function(data) {
 				// FIXME
 				console.log(data);
+			});		
+		},
+		
+		loadAnimes: function() {
+			var storedData = localStorage.getItem('animes');		
+			if (storedData) {
+				try {
+					var animes = JSON.parse(storedData);
+					/*
+						Each anime receives two lists:
+						- one for torrents that are recognized as being 'new' (have an episode number above the currently saved one)
+						- one for torrents that did not get into the 'new' list
+						Both lists are initialized with an empty array because they're loaded via ng-init.
+					*/
+					for (var i = 0; i < animes.length; i++) {
+						animes[i].nextLinks = [];
+						animes[i].latestLinks = [];
+					}
+					
+					return animes;
+				} catch (e) {
+					// ignored
+				}
+			}
+			
+			return [];
+		},
+		
+		storeAnimes: function(animes) {
+			// TODO: use angular.copy?
+			var storedAnimes = [];
+			for (var i = 0; i < animes.length; i++) {
+				storedAnimes.push({
+					name: animes[i].name,
+					episode: animes[i].episode,
+					customGroup: animes[i].customGroup
+				});
+			}
+			
+			localStorage.setItem('animes', JSON.stringify(storedAnimes));
+		},
+		
+		requestAnimeFromCurrentPage: function(fn) {
+			chrome.tabs.getSelected(null, function(tab) {
+				chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(anime) {
+					if (anime) {
+						fn(anime);
+					}
+				});
 			});
+		}
+	}
+});
+
+var NyaaCtrl = function($scope, nyaaService) {
+	$scope.animes = nyaaService.loadAnimes();
+	
+	// This model is bound to the create/edit anime form.
+	$scope.animeFormObj = {
+		name: '',
+		episode: 0,
+		customGroup: ''
+	};
+	
+	$scope.showAnimeForm = function(data) {
+		var formObj = $scope.animeFormObj;
+		
+		formObj.name = data.name;
+		formObj.episode = data.episode;
+		formObj.customGroup = data.customGroup;
+		
+		document.getElementById('animeForm').style.display = '';	
+	};
+	$scope.createAnime = function() {
+		$scope.showAnimeForm({
+			name: '',
+			episode: 0,
+			customGroup: ''
+		});	
+	};
+	
+	nyaaService.requestAnimeFromCurrentPage(function(anime) {
+		console.log(anime);
+		$scope.showAnimeForm(anime);
+	});
+};
+
+var AnimeManager = function($scope, nyaaService) {
+	/*
+		Opens a new tab to view a specific anime in anidb.net.
+	*/
+	$scope.openAnidb = function(name) {
+		var url = 'http://anidb.net/perl-bin/animedb.pl?show=animelist&adb.search=' + escape(name) + '&do.search=search';
+		
+		chrome.tabs.update(null, { url: url });		
+	};
+	
+	/*
+		Moves an anime (identified by its index) using an offset.
+	*/
+	$scope.move = function(idx, offset) {
+		var animes = $scope.animes;
+		if ((idx + offset) >= 0 && ((idx + offset) < animes.length)) {
+			var curr = animes[idx];
+				
+			animes.splice(idx, 1);
+			animes.splice(idx + offset, 0, curr);
+			
+			nyaaService.storeAnimes($scope.animes);
+		}
+	};
+	
+	/*
+		Removes an anime (identified by its index).
+	*/
+	$scope.remove = function(idx) {
+		if (confirm('Really?')) {
+			$scope.animes.splice(idx, 1);
+			nyaaService.storeAnimes($scope.animes);
+		}
+	};
+	
+	/*
+		Increases the episode counter by one.
+	*/
+	$scope.plusOne = function(anime) {
+		anime.episode += 1;
+		
+		nyaaService.storeAnimes($scope.animes);
+		window.location.reload();
+	};
+	
+	/*
+		Pulls the torrents for a specific anime from nyaa.eu and appends the links 
+		to anime.nextLinks and anime.latestLinks.
+	*/
+	$scope.pullTorrentList = function(anime) {
+		nyaaService.queryTorrents(anime, function(links) {
+			var nextList = anime.nextLinks,
+				latestList = anime.latestLinks,
+				maxLatest = 15,
+				maxNext = 3,
+				scanEpisode = function(title) {
+					(/\-[ _]([0-9]*).*/g).exec(title);
+	
+					if (RegExp.$1) {
+						return parseInt(RegExp.$1, 10);
+					} else {
+						return 0;
+					}
+				};
+				
+			for (var i = 0; i < links.length && nextList.length < maxNext && latestList.length < maxLatest; i++) {
+				var link = {
+					title: links[i].querySelector('title').firstChild.nodeValue,
+					url: links[i].querySelector('link').firstChild.nodeValue
+				};			
+				
+				if (nextList.length < maxNext && scanEpisode(link.title) > anime.episode) {
+					nextList.push(link);
+				} else {
+					latestList.push(link);
+				}
+			}			
+		});
 	};
 	
 	$scope.toggleLatest = function(event) {
@@ -88,137 +203,33 @@ var TorrentCtrl = function($scope, $http) {
 	$scope.openTorrent = function(url) {
 		chrome.tabs.update(null, { url: url });
 	};
-	
-	$scope.showAnimeForm = function(data) {
-		var formObj = $scope.animeFormObj;
-		
-		formObj.name = data.name;
-		formObj.episode = data.episode;
-		formObj.customGroup = data.customGroup || data.group;
-		
-		document.getElementById('animeForm').style.display = '';
-	};
-	$scope.editAnime = function(anime) {
-		showAnimeForm(anime);
-	};
-	$scope.createAnime = function() {
-		showAnimeForm({
-			name: '',
-			episode: 0,
-			customGroup: ''
-		});
+};
+
+var EditAnime = function($scope, nyaaService) {	
+	var getAnimeByName = function(animes, name) {
+		for (var i = 0; i < animes.length; i++) {
+			if (animes[i].name === name) {
+				return animes[i];
+			}
+		}
 	};
 	
 	$scope.saveAnime = function() {
 		var formObj = $scope.animeFormObj,
-			anime = getAnimeByName(animes, formObj.name);
-			
+			anime = getAnimeByName($scope.animes, formObj.name);
+		
 		if (!anime) {
 			anime = {
 				name: formObj.name
 			};
-			animes.push(anime);
+			$scope.animes.push(anime);
 		}
 		
 		anime.episode = parseInt(formObj.episode, 10);
-		anime.customGroup = formObj.customGroup;
+		anime.customGroup = formObj.customGroup || '';
 		
-		storeAnimes($scope.animes);
+		nyaaService.storeAnimes($scope.animes);
 		window.location.reload();
 	};
-	
-	requestAnidb($scope);
-};
-	
-// FIXME: move into controller
-function getAnimeByName(animes, name) {
-	for (var i = 0; i < animes.length; i++) {
-		if (animes[i].name === name) {
-			return animes[i];
-		}
-	}
 };
 
-// FIXME: move into controller
-function requestAnidb(scope) {
-	chrome.tabs.getSelected(null, function(tab) {
-		chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(anime) {
-			if (anime) {
-				if (!getAnimeByName(scope.animes, anime.name)) {
-					scope.showAnimeForm({
-						name: anime.name,
-						// FIXME: re-support this feature groups: anime.groups,
-						customGroup: '',
-						episode: 0
-					});
-				}
-			}			
-		});
-	});
-};
-
-/*
- * Storage and local data management
- */
-// FIXME: move into controller
-function storeAnimes(animes) {
-	// TODO: use angular.copy?
-	var storedAnimes = [];
-	for (var i = 0; i < animes.length; i++) {
-		storedAnimes.push({
-			name: animes[i].name,
-			episode: animes[i].episode,
-			customGroup: animes[i].customGroup
-		});
-	}
-	
-	localStorage.setItem('animes', JSON.stringify(storedAnimes));
-};
-// FIXME: move into controller
-function loadAnimes() {
-	var storedData = localStorage.getItem('animes');		
-	if (storedData) {
-		try {
-			return JSON.parse(storedData);
-		} catch (e) {
-			// ignored
-		}
-	}
-	
-	return [];
-};
-
-/*
- * Connector to nyaa.eu
- */
-// FIXME: move into controller
-function getEpisode(title) {
-	(/\-[ _]([0-9]*).*/g).exec(title);
-	
-	if (RegExp.$1) {
-		return parseInt(RegExp.$1, 10);
-	} else {
-		return 0;
-	}
-};
-
-// FIXME: move into controller
-function insertLinks(xmlItems, anime) {	
-	var nextList = anime.nextLinks,
-		latestList = anime.latestLinks,
-		maxLatest = 15,
-		maxNext = 3;
-		
-	for (var i = 0; i < xmlItems.length && nextList.length < maxNext && latestList.length < maxLatest; i++) {
-		var link = {
-			title: xmlItems[i].querySelector('title').firstChild.nodeValue,
-			url: xmlItems[i].querySelector('link').firstChild.nodeValue
-		};			
-			
-		if (nextList.length < maxNext && getEpisode(link.title) > anime.episode) {
-			nextList.push(link);
-		} else {
-			latestList.push(link);
-		}
-	}
-};
